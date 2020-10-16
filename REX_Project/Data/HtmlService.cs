40498d01f094859ba562f123357b5be52f2475a1
Mohammadjavad Esfandiari <mesfandi@masonlive.gmu.edu>
2020-10-16 12:00:09 -0400

    This is a sample project. completed v1

---
+++ REX_Project/Data/HtmlService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REX_Project.Data
+{
+    public class HtmlService
+    {
+        [Parameter]
+        public string Body { get; set; } = "asda";
+        public event Action OnChange;
+        public string getBody()
+        {
+            return Body;
+        }
+        public void setBody(string B)
+        {
+            Body=B;
+            NotifyStateChanged();
+        }
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+
+    }
+}
